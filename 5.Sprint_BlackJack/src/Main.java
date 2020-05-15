775ae49845bd57ba602cfcb7c7c5ec8c9e90b94e
Grete Veetla <greteveetla@gmail.com>
2020-05-15 15:09:22 +0300

    pelaaja saa melkein jo kolme korttia

--- 5.Sprint_BlackJack/src/Main.java
+++ 5.Sprint_BlackJack/src/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 //        Kortti k = new Kortti(3, "diamonds");
 //        System.out.println(k);
         
-    Korttipakka pakka = new Korttipakka();
+//    Korttipakka pakka = new Korttipakka();
         
 
 //    BlackJack bj = BlackJack();
